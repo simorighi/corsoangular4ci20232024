0b0216d9b816d7de7b131af644fee6f5268f4913
simorighi <113978586+simorighi@users.noreply.github.com>
2024-02-20 10:36:46 +0000

    creazione nuovi components

---
+++ spotify/src/app/albums/album-grid/album-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './album-grid.component.html',
+  styleUrl: './album-grid.component.css'
+})
+export class AlbumGridComponent {
+
+}
