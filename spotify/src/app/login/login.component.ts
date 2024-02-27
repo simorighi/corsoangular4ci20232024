3976dc57b5681bc1723a0ae3765c96c359895000
simorighi <113978586+simorighi@users.noreply.github.com>
2024-02-27 11:53:20 +0000

    able and disable login button

--- spotify/src/app/login/login.component.ts
+++ spotify/src/app/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  //importp la libreria per la gestione dei form reactive
+  //possibilità di controllare i dati del form usando ts
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+     username: new FormControl('', [Validators.required, Validators.email]),
+     password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+
+  
 
 }
